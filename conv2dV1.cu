ca43450e10e633a8c4fa26add614540ee5328c81
Rajlee Dhabekar <58782667+Rajlee3107@users.noreply.github.com>
2025-04-13 23:07:55 -0400

    Add files via upload

---
+++ conv2dV1.cu
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cuda.h>
+#include <random>
+#include <fstream>
+#include <iomanip>
+
+
+// The cuda kernel without shared memory optimization
+__global__ void conv2d_kernel() {
+
+}
+
+
+int main(int argc, char *argv[]) {
+
+  // Read the inputs from command line
+
+  // Allocate/move data using cudaMalloc and cudaMemCpy
+
+  // Launch the kernel
+
+  // Print the output
+
+  // Clean up the memory
+
+  return 0;
+}
---
+++ conv2dV2.cu
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cuda.h>
+#include <random>
+#include <fstream>
+#include <iomanip>
+
+
+// The cuda kernel with shared memory optimization
+// __shared__ float myarr[123];
+__global__ void conv2d_kernel() {
+
+}
+
+
+int main(int argc, char *argv[]) {
+
+  // Read the inputs from command line
+
+  // Allocate/move data using cudaMalloc and cudaMemCpy
+
+  // Launch the kernel
+
+  // Print the output
+
+  // Clean up the memory
+
+  return 0;
+}
